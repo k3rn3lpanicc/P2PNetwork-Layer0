6e9c3dbcf72defaf0735d78d7559d6033027d051
Matin <matin.ghiasvand1381@gmail.com>
2022-08-25 18:12:28 +0430

    Added ppacket struct and made some methods for it(to calculate paylod's hash and save in checksum)

--- layer0/src/main.rs
+++ layer0/src/main.rs
@@ -1,7 +1,12 @@
-mod sha3;
-use sha3::{get_hash_str, get_hash};
-
+mod hashing;
+mod ppacket;
+use hashing::{get_hash_str, get_hash};
+use ppacket::PPacket;
 fn main() {
     println!("Sha3('abc') = {}", get_hash_str("abc"));
     println!("Sha3('abc') = {}", get_hash("abc".as_bytes())); // same as above
+    let a : PPacket = PPacket::from_str(1, "salam");
+    let b : PPacket = PPacket::new(1, "salam".as_bytes());
+    println!("A's checksum : {}" , a.checksum);
+    println!("B's checksum : {}" , b.checksum);
 }
---
+++ layer0/src/ppacket.rs
@@ -0,0 +1,35 @@
+use crate::hashing;
+
+
+pub struct PPacket{
+    pub command : u64,
+    pub payload_size : u32,
+    pub checksum : String,
+    pub payload : Vec<u8>,
+}
+impl PPacket{
+    pub fn new(command : u64, payload : &[u8])->PPacket{
+        let payload_size = payload.len() as u32;
+        let checksum = hashing::get_hash(payload);
+        PPacket{
+            command,
+            payload_size,
+            checksum,
+            payload: payload.to_vec(),
+        }
+    }
+    pub fn from_str(command : u64, payload : &str)->PPacket{
+        let payload_size = payload.len() as u32;
+        let checksum = hashing::get_hash(payload.as_bytes());
+        PPacket{
+            command,
+            payload_size,
+            checksum,
+            payload: payload.as_bytes().to_vec(),
+        }
+    }
+    pub fn get_checksum(&self)->String{
+        hashing::get_hash(&self.payload)
+    }
+
+}
