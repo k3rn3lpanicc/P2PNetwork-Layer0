b4d9e77106c650824199168109bce685173bf0f5
Matin <matin.ghiasvand1381@gmail.com>
2022-08-25 23:16:19 +0430

    minor changes

--- layer0/src/main.rs
+++ layer0/src/main.rs
@@ -3,6 +3,7 @@ mod ppacket;
 mod client;
 use ppacket::PPacket;
 use tokio::net::{TcpListener, TcpStream};
+use std::time::Duration;
 
 const PORT_NUMBER : u16 = 1234;
 #[tokio::main]
@@ -22,10 +23,10 @@ async fn main() {
     else{
         println!("Failed to bind, check port availability"); 
     }    
-
     let mut stream : TcpStream = TcpStream::connect("0.0.0.0:1234").await.unwrap();
+    tokio::time::sleep(Duration::from_secs(3)).await;
     client::send_ppacket(&mut stream, &PPacket::from_str(123,"abc")).await;
     println!("Sent {}" , PPacket::from_str(123,"abc").get_checksum());
-
+    
     
 }
