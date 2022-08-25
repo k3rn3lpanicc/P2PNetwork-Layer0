b21387e5eb3905e5b8a7bce9dd8d9cfd91206180
Matin <matin.ghiasvand1381@gmail.com>
2022-08-25 17:42:40 +0430

    added sha3 hashing functionality

--- layer0/src/main.rs
+++ layer0/src/main.rs
@@ -1,3 +1,7 @@
+mod sha3;
+use sha3::{get_hash_str, get_hash};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Sha3('abc') = {}", get_hash_str("abc"));
+    println!("Sha3('abc') = {}", get_hash("abc".as_bytes())); // same as above
 }
---
+++ layer0/src/sha3.rs
@@ -0,0 +1,19 @@
+
+use crypto::digest::Digest;
+use crypto::sha3::Sha3;
+
+
+pub fn get_hash(bytes : &[u8])->String{
+    let mut hasher = Sha3::sha3_256();
+    //hasher.input_str(bytes);
+    hasher.input(bytes);
+    let result = hasher.result_str();
+    result
+}
+pub fn get_hash_str(data : &str)->String{
+    let mut hasher = Sha3::sha3_256();
+    //hasher.input_str(bytes);
+    hasher.input_str(data);
+    let result = hasher.result_str();
+    result
+}
